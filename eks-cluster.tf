3d16a7288fca1e7f1fbaaf88ce71c93582f0b15f
Robert Stapleton <robert@stapleton-family.net>
2023-03-17 09:30:20 -0400

    cluster worker node config

---
+++ eks-cluster.tf
@@ -0,0 +1,27 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.5.1"
+
+  cluster_name    = "myapp-eks-cluster"
+  cluster_version = "1.22"
+
+  # network for the worker nodes to start in - the workload
+  # private subnets as an array
+  subnet_ids = module.myapp-vpc.private_subnets
+  vpc_id     = module.myapp-vpc.vpc_id
+
+  tags = {
+    environment = "dev"
+    application = "myapp"
+  }
+
+  eks_managed_node_groups = {
+    dev = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 3
+
+      instance_types = ["t2.small"]
+    }
+  }
+}
