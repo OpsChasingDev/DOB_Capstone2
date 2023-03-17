2a91666e1b7351706b43d2c5b54817284742db7c
Robert Stapleton <robert@stapleton-family.net>
2023-03-17 09:49:14 -0400

    k8s provider with auth

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,3 +1,18 @@
+provider "kubernetes" {
+  load_config_file       = false
+  host                   = data.aws_eks_cluster.myapp-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp-cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.myapp-cluster.token
+}
+
+data "aws_eks_cluster" "myapp-cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "myapp-cluster" {
+  name = module.eks.cluster_id
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.5.1"
